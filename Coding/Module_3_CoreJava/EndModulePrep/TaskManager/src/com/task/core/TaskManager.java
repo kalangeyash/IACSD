ccd6562944693afde46fece328837a1f875bfd84
Yash Kalange <yashkalange127@gmail.com>
2025-09-29 23:27:27 +0530

    started new case study:taskmanager

---
+++ Coding/Module_3_CoreJava/EndModulePrep/TaskManager/src/com/task/core/TaskManager.java
@@ -0,0 +1,27 @@
+package com.task.core;
+/*
+ . Lab exam difficulty level
+Create a menu driven application "TaskManager" to manage your day-to-day tasks. 
+
+You can create a class Task with fields like taskId, taskName, description, taskDate, status, active. 
+taskId should be unique and generated automatically.
+status should be either PENDING, IN PROGRESS or COMPLETED.
+active should be either true or false. Deleted task will have active=false 
+Newly added task should have default status as PENDING and active=true
+
+You can use suitable data structure to store data in memory.
+
+Following functionalities are expected -
+
+a. Add New Task                       
+b. Delete a task                         
+c. Update task status               
+d. Display all pending tasks     
+e. Display all pending tasks for today           
+f.  Display all tasks sorted by taskDate            
+
+ **/
+
+public class TaskManager {
+
+}
