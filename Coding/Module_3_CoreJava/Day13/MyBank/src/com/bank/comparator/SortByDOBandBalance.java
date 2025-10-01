169b4c410fae6224e4c6db8c6e11b25b6600bd7c
Yash Kalange <yashkalange127@gmail.com>
2025-10-01 22:44:28 +0530

    changed bank comparator

--- Coding/Module_3_CoreJava/Day13/MyBank/src/com/bank/comparator/SortByDOBandBalance.java
+++ Coding/Module_3_CoreJava/Day13/MyBank/src/com/bank/comparator/SortByDOBandBalance.java
@@ -10,7 +10,7 @@ public class SortByDOBandBalance implements Comparator<BankAccount>{
 	public int compare(BankAccount o1, BankAccount o2) {
 		// TODO Auto-generated method stub
 		int type =  o1.getDob().compareTo(o2.getDob());
-		
+		if (type == 0)
 		if(type == 0)
 		{
 			return Double.compare(o1.getBalance(), o2.getBalance());
